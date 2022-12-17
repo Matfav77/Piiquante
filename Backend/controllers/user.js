51538d61cb6a362128d69f0bf4a05d4598ab33a3
Mathieu Favre <matfav@hotmail.fr>
2022-12-17 11:47:28 +0100

    Added login logic, split into controller and route, with token generation

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { findOne } = require("../models/user");
 
 exports.signup = async (req, res) => {
     const { email, password } = req.body;
@@ -8,4 +10,29 @@ exports.signup = async (req, res) => {
     newUser.save()
         .then(() => { res.status(200).json({ message: "Utilisateur créé !" }) })
         .catch(err => res.status(401).json({ err }))
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(401).json({ message: "Paire identifiant/mot de passe invalide." })
+        else {
+            isValid = await bcrypt.compare(password, user.password)
+            if (!isValid) return res.status(401).json({ message: "Paire identifiant/mot de passe invalide." })
+            else {
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                    )
+                })
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+
 }
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authCtrl = require("../controllers/user")
 
 router.post("/signup", authCtrl.signup);
+router.post("/login", authCtrl.login)
 
 module.exports = router;
