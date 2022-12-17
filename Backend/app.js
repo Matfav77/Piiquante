9dba15dafb1195293e46482a730088b89f450404
Mathieu Favre <matfav@hotmail.fr>
2022-12-17 11:07:04 +0100

    Added express server setup and DB connection to app

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+mongoose.connect("mongodb+srv://Matfav:0penClassrooms@cluster0.huhxfny.mongodb.net/?retryWrites=true&w=majority");
+
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
+
