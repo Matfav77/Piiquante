a614481a305e17121e2debb7269e262150ad240c
Mathieu Favre <matfav@hotmail.fr>
2022-12-17 12:47:44 +0100

    Installed multer and set up its configuration middleware, which will save images in the 'images' folder

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
