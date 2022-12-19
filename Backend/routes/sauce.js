4d4d58063216c1547d782994c5bddd5ce8763057
Mathieu Favre <matfav@hotmail.fr>
2022-12-19 16:27:42 +0100

    Added auth check on all sauce routes.

--- Backend/routes/sauce.js
+++ Backend/routes/sauce.js
@@ -5,10 +5,10 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
 
-router.get("/", sauceCtrl.getAllSauces);
+router.get("/", auth, sauceCtrl.getAllSauces);
 router.post("/", auth, multer, sauceCtrl.createSauce)
 
-router.get("/:id", sauceCtrl.getOneSauce)
+router.get("/:id", auth, sauceCtrl.getOneSauce)
 router.put("/:id", auth, multer, sauceCtrl.modifySauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 
